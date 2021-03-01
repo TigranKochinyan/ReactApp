e3725d5ceb9a69b0756b041ef76a70f3ddb3313b
TigranKochinyan <tigrankochinyan24@gmail.com>
2021-03-01 16:52:14 +0400

    add search end sort inputs and add in state for fetching

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,9 +2,15 @@ export function fromatingDate(dateString="") {
     return dateString.slice(0, 10);
 }
 
-export function cutText(text, size=60) {
+export function cutText(text = '', size = 60) {
     if( text.length < size ) {
         return text;
     }
     return `${text.slice(0,size)}...`;
+}
+
+export function textTransform(text) {
+    if (typeof text !== 'string') return '';
+    let edited = text.charAt(0).toUpperCase() + text.slice(1);
+    return edited.split('_').join(' ');
 }
