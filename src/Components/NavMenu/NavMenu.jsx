58af569e27e7f87b97ef4c4c30772aed747ecf79
TigranKochinyan <tigrankochinyan24@gmail.com>
2021-03-15 14:11:48 +0400

    add input fields and validation to Login and Registe pages, fix header always opened bug , edit styles

--- src/Components/NavMenu/NavMenu.jsx
+++ src/Components/NavMenu/NavMenu.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 import './navMenu.scss';
 
 const NavMenu = () => {
+    const [navMenuShow, setNavMenuShow] = useState(false)
+    const showHideNavMenu = () => {
+        setNavMenuShow(!navMenuShow);
+    };
     return (
-        <Navbar className="navMenu" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar className="navMenu" expanded={navMenuShow}  expand="lg" bg="dark" variant="dark">
             <Navbar.Brand> 
-                <Link to="/"> To Do </Link>
+                <Link onClick={showHideNavMenu} className="navMenu-link" to="/"> To Do </Link>
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Toggle onClick={showHideNavMenu} aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto">
-                    <NavLink activeClassName="navMenu-active-link" className="navMenu-link" to="/contact"> Contact Us </NavLink>
-                    <NavLink activeClassName="navMenu-active-link" className="navMenu-link" to="/about"> About </NavLink>
+                    <NavLink activeClassName="navMenu-active-link" className="navMenu-link" onClick={showHideNavMenu} to="/contact"> Contact Us </NavLink>
+                    <NavLink activeClassName="navMenu-active-link" className="navMenu-link" onClick={showHideNavMenu} to="/about"> About </NavLink>
+                    <NavLink activeClassName="navMenu-active-link" className="navMenu-link" onClick={showHideNavMenu} to="/signin"> Sign in </NavLink>
+                    <NavLink activeClassName="navMenu-active-link" className="navMenu-link" onClick={showHideNavMenu} to="/signup"> Sign Up </NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-    )
-}
+    );
+};
 
 export default NavMenu;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import './pages.scss';
 
+const Login = () => {
+    const [ inputValues, setInputValues ] = useState({
+        inputPassword: '',
+        inputEmail: ''
+    });
 
+    const [ inputsIsValid, setInputsIsValid ] = useState({
+        inputPassword: null,
+        inputEmail: null
+    });
 
-const Login = (props) => {
+    const handleChange = ({target: {name, value}}) => {
+        setInputValues({
+            ...inputValues,
+            [name]: value
+        });
+        validator(name, value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const errorArr = Object.values(inputsIsValid);
+        const errorExist = !errorArr.every(el => el === null);//true when errors null
+        const inputValuesArr = Object.values(inputValues);
+        const inputValuesEmpty = inputValuesArr.some(el => el === '');//true when some inputs is empty 
+
+        if(!errorExist && inputValuesEmpty){//if submit without input changes
+            setInputsIsValid({
+                inputPassword: (inputValues.inputPassword === '' ? 'field is required' : null),
+                inputEmail: (inputValues.inputEmail === '' ? 'field is required' : null),
+            })
+            return;
+        }
+        if(!errorExist && !inputValuesEmpty){
+            console.log(inputValues);//fetch data to server
+        };
+    };
+
+    const validator = (name, value) => {
+        let notiication = null;
+        if(value.trim() === '') {
+            notiication = 'field is required';
+        }
+        if(name === 'inputEmail' && !(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/).test(value)){
+            notiication = 'Please entet valid email';
+        }
+        if(name === 'inputPassword' && value.trim() !== '') {
+            if(!(/(?=.*[a-z])/).test(value)){
+                notiication = 'password should contain at least one lowercase letter';
+            };
+            if(!(/(?=.*[A-Z])/).test(value)){
+                notiication = 'password should contain at least one uppercase letter';
+            };
+            if(value.length < 8){
+                notiication = 'password should more then 8 charackters';
+            };
+        }
+        setInputsIsValid({
+            ...inputsIsValid,
+            [name]: notiication
+        });
+    }
+
     return (
-        <>
-            Login page
-        </>
-    )
-}
+        <Container className="signin">
+            <Row className="signin-row">
+                <Col lg={6} md={8} xs={12}>
+                    <h1>Log in</h1>
+                    <Form>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control 
+                                className="contact-input"
+                                onChange={handleChange}
+                                value={inputValues.inputEmail}
+                                name="inputEmail" 
+                                type="email" 
+                                placeholder="email" 
+                                />
+                            <Form.Text className="text-danger">
+                                {inputsIsValid.inputEmail}
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control 
+                                className="contact-input"
+                                onChange={handleChange}
+                                value={inputValues.inputPassword}
+                                name="inputPassword"
+                                type="password"
+                                placeholder="Enter your password"
+                                />
+                            <Form.Text className="text-danger">
+                                {inputsIsValid.inputPassword}
+                            </Form.Text>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit}>
+                            Sign in
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default Login;
